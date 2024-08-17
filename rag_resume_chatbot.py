e92b03e17a41d1053b033f5759643c16fe96a991
ivanamati <107301280+ivanamati@users.noreply.github.com>
2024-08-17 20:43:00 +0200

    Update rag_resume_chatbot.py

--- rag_resume_chatbot.py
+++ rag_resume_chatbot.py
@@ -18,7 +18,7 @@ def rag(open_api_key):
     # Embedd the chunks and load it into vector store
     embeddings = OpenAIEmbeddings(api_key=open_api_key)
     # Pass the documents and embeddings inorder to create FAISS vector index
-    vectorindex_openai = FAISS.from_documents(documents, embeddings)
+    vectorindex_openai = FAISS.from_texts(documents, embeddings)
     # Save the vectorstore object locally
     vectorindex_openai.save_local("vectorindex_openai")
     # Load the vectorstore object
