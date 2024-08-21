1ad2d5f8c24686a9a6e37f105ee493d659978820
Ivana <ivana@silverbrain.ai>
2024-08-21 15:26:19 +0200

    add timer

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title("'IvyBot'")
 st.markdown("### Ivana's AI Assistant")
 st.markdown("""IvyBot is here to assist Ivana in her job search by providing recruiters 
             with relevant information, but also showing her developing skills employing newest AI technology.""")
-
+st.status
 company_name = st.sidebar.text_input('Company (recruiter) Name')
 openai_api_key = st.sidebar.text_input('OpenAI API key')
 
@@ -21,6 +21,19 @@ with st.form('my_form'):
   if not company_name:
     st.warning('Please enter your name or company name', icon='⚠')
   if submitted and openai_api_key.startswith('sk-') and company_name: 
+    main_placeholder = st.empty()
+    main_placeholder.text("IvyBot is thinking...")
+    import time
+
+    with st.status("IvyBot is thinking...", expanded=True) as status:
+        st.write("Searching for data...")
+        time.sleep(2)
+        st.write("Found data")
+        time.sleep(1)
+        st.write("Retrieving the data...")
+        time.sleep(1)
+        status.update(label="Answer found!", state="complete", expanded=False)
+        
   #if submitted:
     response = rag_resume_chatbot.generate_answer(text, openai_api_key)
     st.info(response)
@@ -34,3 +47,4 @@ st.sidebar.info("✉️ ivanamati@gmail.com")
 st.sidebar.markdown("##### Ivana's Coding Projects:")
 st.sidebar.info("https://www.linkedin.com/in/ivanamaticphd/ (Projects Section)")
 st.sidebar.info("https://github.com/ivanamati")
+
